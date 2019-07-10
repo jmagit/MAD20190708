bc7dc4733cfb89f8c6d17a3adcea7e0851f2c578
Javier <jmartin@grupoloyal.com>
2019-07-10 13:43:39 +0200

    Demos

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NotificationComponent } from './notification/notification.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, NotificationComponent],
+  exports: [HomeComponent, NotificationComponent],
   imports: [
     CommonModule
   ]
--- src/indra-core/index.ts
+++ src/indra-core/index.ts
@@ -1,2 +1,3 @@
 export { IndraCoreModule } from './indra-core.module';
 export * from './services/logger.service';
+export { ElipsisPipe } from './pipes/cadenas.pipe';
