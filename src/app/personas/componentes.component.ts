1ced25aa9708c73d286ce9bea4813316f914ba7d
Javier <jmartin@grupoloyal.com>
2019-07-11 12:40:41 +0200

    Codigo

--- src/app/personas/componentes.component.ts
+++ src/app/personas/componentes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonasViewModelService } from './servicios.service';
 
 @Component({
   selector: 'app-personas',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estilos.component.css']
 })
 export class PersonasComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private vm: PersonasViewModelService) { }
+  public get VM() { return this.vm; }
   ngOnInit() {
   }
-
 }
+
+@Component({
+  selector: 'app-personas-list',
+  templateUrl: './template-list.component.html',
+  styleUrls: ['./estilos.component.css']
+})
+export class PersonasListComponent implements OnInit {
+  constructor(private vm: PersonasViewModelService) { }
+  public get VM() { return this.vm; }
+  ngOnInit() {
+  }
+}
+
+@Component({
+  selector: 'app-personas-add',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./estilos.component.css']
+})
+export class PersonasAddComponent implements OnInit {
+  constructor(private vm: PersonasViewModelService) { }
+  public get VM() { return this.vm; }
+  ngOnInit() {
+  }
+}
+
+@Component({
+  selector: 'app-personas-edit',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./estilos.component.css']
+})
+export class PersonasEditComponent implements OnInit {
+  constructor(private vm: PersonasViewModelService) { }
+  public get VM() { return this.vm; }
+  ngOnInit() {
+  }
+}
+
+@Component({
+  selector: 'app-personas-view',
+  templateUrl: './template-view.component.html',
+  styleUrls: ['./estilos.component.css']
+})
+export class PersonasViewComponent implements OnInit {
+  constructor(private vm: PersonasViewModelService) { }
+  public get VM() { return this.vm; }
+  ngOnInit() {
+  }
+}
+
+export const PERSONAS_COMPONENTS = [ PersonasComponent, PersonasListComponent, PersonasAddComponent,
+  PersonasEditComponent, PersonasViewComponent];
