beb8c872a3e4e02035aa46f638394ae0f933f89f
Javier <35864946+jmagit@users.noreply.github.com>
2019-07-12 13:21:54 +0200

    router

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotificationComponent } from './notification/notification.component';
 import { AjaxWaitComponent } from './ajax-wait';
+import { MenuComponent } from './menu/menu.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [HomeComponent, NotificationComponent, AjaxWaitComponent],
-  exports: [HomeComponent, NotificationComponent, AjaxWaitComponent],
+  declarations: [HomeComponent, NotificationComponent, AjaxWaitComponent, MenuComponent, PageNotFoundComponent],
+  exports: [HomeComponent, NotificationComponent, AjaxWaitComponent, MenuComponent, PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
   ]
 })
 export class MainModule { }
