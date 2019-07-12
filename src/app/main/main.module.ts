c1519b49abc8056e648bd5def0ad60c767891926
Javier <35864946+jmagit@users.noreply.github.com>
2019-07-12 11:03:13 +0200

    http

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotificationComponent } from './notification/notification.component';
+import { AjaxWaitComponent } from './ajax-wait';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, NotificationComponent],
-  exports: [HomeComponent, NotificationComponent],
+  declarations: [HomeComponent, NotificationComponent, AjaxWaitComponent],
+  exports: [HomeComponent, NotificationComponent, AjaxWaitComponent],
   imports: [
     CommonModule
   ]
